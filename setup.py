9061c5c330698d68eaca981f03b107e8e45f3b0d
Antonio López Rivera <47611105+alopezrivera@users.noreply.github.com>
2021-09-09 14:27:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpl_plotter",
-    version="4.0.3",
+    version="4.0.4",
     author="Antonio Lopez Rivera",
     author_email="antonlopezr99@gmail.com",
     description="Matplotlib-based plotting library",
@@ -19,7 +19,7 @@ setuptools.setup(
         "scikit-image",
         "pandas",
         "PyQt5",
-        "Python-Alexandria",
+        "Python-Alexandria>=2.0.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
