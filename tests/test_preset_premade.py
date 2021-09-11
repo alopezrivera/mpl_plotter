163f2ff45a40d3cefdae4d3e4d19a03416dda2de
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-11 22:11:03 +0200

    Updated test

--- tests/test_preset_premade.py
+++ tests/test_preset_premade.py
@@ -4,7 +4,7 @@
 import unittest
 import numpy as np
 
-from mpl_plotter.color.schemes import one
+from mpl_plotter.color.schemes import colorscheme_one
 
 
 x = np.linspace(0, 4, 1000)
@@ -19,33 +19,33 @@ class Tests(unittest.TestCase):
 
     def test_publication2(self):
         from mpl_plotter.presets.publication import two_d
-        two_d.line(x, z, color=one()[5], show=show, backend=backend)
+        two_d.line(x, z, color=colorscheme_one()[5], show=show, backend=backend)
         two_d.scatter(x, z, norm=z, show=show, backend=backend)
-        two_d.heatmap(color=one()[5], show=show, backend=backend)
-        two_d.quiver(color=one()[5], show=show, backend=backend)
-        two_d.streamline(color=one()[5], show=show, backend=backend)
-        two_d.fill_area(color=one()[5], show=show, backend=backend)
+        two_d.heatmap(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.quiver(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.streamline(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.fill_area(color=colorscheme_one()[5], show=show, backend=backend)
 
     def test_precision2(self):
         from mpl_plotter.presets.precision import two_d
-        two_d.line(x, z, color=one()[5], show=show, backend=backend)
+        two_d.line(x, z, color=colorscheme_one()[5], show=show, backend=backend)
         two_d.scatter(x, z, norm=z, show=show, backend=backend)
-        two_d.heatmap(color=one()[5], show=show, backend=backend)
-        two_d.quiver(color=one()[5], show=show, backend=backend)
-        two_d.streamline(color=one()[5], show=show, backend=backend)
-        two_d.fill_area(color=one()[5], show=show, backend=backend)
+        two_d.heatmap(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.quiver(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.streamline(color=colorscheme_one()[5], show=show, backend=backend)
+        two_d.fill_area(color=colorscheme_one()[5], show=show, backend=backend)
 
 
 class Tests3(unittest.TestCase):
 
     def test_publication3(self):
         from mpl_plotter.presets.publication import three_d
-        three_d.line(x, y, z, color=one()[5], show=show, backend=backend)
-        three_d.scatter(x, y, z, norm=z, color=one()[5], show=show, backend=backend)
-        three_d.surface(color=one()[5], show=show, backend=backend, demo_pad_plot=True)
+        three_d.line(x, y, z, color=colorscheme_one()[5], show=show, backend=backend)
+        three_d.scatter(x, y, z, norm=z, color=colorscheme_one()[5], show=show, backend=backend)
+        three_d.surface(color=colorscheme_one()[5], show=show, backend=backend, demo_pad_plot=True)
 
     def test_precision3(self):
         from mpl_plotter.presets.precision import three_d
-        three_d.line(x, y, z, color=one()[5], show=show, backend=backend)
-        three_d.scatter(x, y, z, norm=z, color=one()[5], show=show, backend=backend)
-        three_d.surface(color=one()[5], show=show, backend=backend, demo_pad_plot=True)
+        three_d.line(x, y, z, color=colorscheme_one()[5], show=show, backend=backend)
+        three_d.scatter(x, y, z, norm=z, color=colorscheme_one()[5], show=show, backend=backend)
+        three_d.surface(color=colorscheme_one()[5], show=show, backend=backend, demo_pad_plot=True)
