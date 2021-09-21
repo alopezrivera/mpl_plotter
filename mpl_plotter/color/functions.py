24addf54dbdc58c6bbf5da3c442c016b39c2372c
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-21 17:18:34 +0200

    Map stack implementation

--- mpl_plotter/color/functions.py
+++ mpl_plotter/color/functions.py
@@ -6,7 +6,8 @@ Color functions
 ---------------
 """
 
-
+import numpy as np
+import matplotlib as mpl
 from matplotlib.colors import to_hex, to_rgba
 
 
@@ -57,3 +58,37 @@ def delta(color, factor, fmt='hex'):
         return to_hex(c_mod)
     elif fmt == 'rgb':
         return c_mod
+
+
+def mapstack(maps,
+             fractions=None,
+             ranges=None):
+    """
+    Create a colormap stacking an arbitrary number of
+    conventional Matplotlib colormaps.
+
+    :param maps:      List of colormap NAMES
+    :param fractions: For each original colormap, the fraction it'll take of the merged colormap.
+                         [0<fr_0<1, ...]
+    :param ranges:    For each original colormap, the range taken.
+                         [(0=<min<1, 0<max<=1)_0, ...]
+
+    :type maps:       list of str
+    :type fractions:  list of float
+    :type ranges:     list of tuple
+
+    :return: mpl.colors.LinearSegmentedColormap
+    """
+
+    if isinstance(fractions, type(None)):
+        fractions = np.ones(len(maps))*1/len(maps)
+
+    if isinstance(ranges, type(None)):
+        ranges = np.tile(np.array([0, 1]), (len(maps), 1))
+
+    stack = tuple(mpl.cm.get_cmap(maps[i])(np.linspace(ranges[i][0], ranges[i][1], int(round(256 * fractions[i], 0))))
+                  for i in range(len(maps)))
+
+    colors = np.vstack(stack)
+
+    return mpl.colors.LinearSegmentedColormap.from_list('custom_RdBu', colors)
