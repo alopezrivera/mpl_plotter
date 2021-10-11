181164e53a96327e1978a1a437e5bd98d8932a9c
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-10-11 15:07:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mpl_plotter",
-    version="4.0.5",
+    version="4.1.0.pre",
     author="Antonio Lopez Rivera",
     author_email="antonlopezr99@gmail.com",
     description="Matplotlib-based plotting library",
