bb1a6f1976b58e1eaa3089c5575a98225a08d239
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-10-14 08:52:56 +0200

    Marker class improved

--- mpl_plotter/__init__.py
+++ mpl_plotter/__init__.py
@@ -47,6 +47,58 @@ def get_available_fonts():
         print(numeral+'"'+names[i]+'"')
 
 
+# SPDX-FileCopyrightText: © 2021 Antonio López Rivera <antonlopezr99@gmail.com>
+# SPDX-License-Identifier: GPL-3.0-only
+
+"""
+Huracan utilities
+-----------------
+"""
+
+import re
+import sys
+import inspect
+
+
+def setattr_namespace(o, namespace):
+    """
+    Set all variables declared in a namespace as as attributes
+    of a class instance.
+    ---
+
+    1. Obtain list of module names
+    2. Get namespace variables
+       - Discard all variables with names starting and ending with '_'
+    3. Create a dictionary of namespace variable names and values
+    4. Set namespace variables as attributes of the input object
+       - Given class instance _o_ will not set as attribute of itself
+       - The parent class of _o_ will not be set as an attribute of _o_
+         if present in the provided namespace.
+
+    :param o: Instance of any given class.
+    :param namespace: A given namespace.
+                      - locals()
+                      - globals()
+
+    :type o: object
+    :type namespace: dict
+    """
+    # List of module names
+    _mods_ = list(set(sys.modules) & set(namespace))
+    # List of function arguments
+    _args_ = setattr_namespace.__code__.co_varnames
+    # Get namespace variables:
+    #    List of local variables which are not special variables nor module names
+    keys = [key for key in namespace.keys() if (key[0] != '_' and key[-1] != '_') and key not in _mods_]
+    # Dictionary of namespace variable names and values
+    vars = {key: namespace[key] for key in keys}
+    for key, value in vars.items():
+        if not type(value) == type(o)\
+                and not isinstance(o, value if inspect.isclass(value) else type(value)):  # Avoid _o_, parent of _o_
+            # Set namespace variables as attributes of the input object
+            setattr(o, key, value)
+
+
 class markers:
     """
     Markers class
@@ -92,6 +144,9 @@ class markers:
     # Garbage tier
     pixel           = ","
 
+    # Icompatible
+    incompatible    = [x]
+
     def __init__(self, hollow=False, plotter='plot'):
         self.hollow  = hollow
         self.plotter = plotter
@@ -102,8 +157,10 @@ class markers:
     def __getitem__(self, item):
 
         special = r'^__(.*?)\__$'
-        keys    = [k for k in markers.__dict__.keys() if not re.match(special, k)]
-        marker  = {'marker': markers.__dict__[keys[item]]}
+
+        m       = {k: markers.__dict__[k] for k in markers.__dict__.keys() if (self.hollow and k not in self.incompatible) or not self.hollow}
+        keys    = [k for k in m.keys() if not re.match(special, k)]
+        marker  = {'marker': m[keys[item]]}
 
         if self.hollow:
             if self.plotter == 'scatter':
@@ -112,4 +169,5 @@ class markers:
                 marker['facecolors'] = 'none'
 
         return marker
+
 
