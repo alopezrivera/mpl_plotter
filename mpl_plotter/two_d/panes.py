94ab2a4762f5e0720330a90b332561551c237bb1
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2022-05-24 17:01:57 +0200

    panes legend issue refactored away

--- mpl_plotter/two_d/panes.py
+++ mpl_plotter/two_d/panes.py
@@ -1,9 +1,9 @@
-# SPDX-FileCopyrightText: © Antonio López Rivera <antonlopezr99@gmail.com>
+# SPDX-FileCopyrightText: © 2021 Antonio López Rivera <antonlopezr99@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 
 """
-Panes
------
+Pane plot method
+----------------
 """
 
 
@@ -19,11 +19,6 @@ from mpl_plotter.two_d import line
 from mpl_plotter.two_d.comparison import comparison
 
 
-"""
-Panes
-=====
-"""
-
 def panes(x,
           y,
           f=None,
@@ -38,91 +33,65 @@ def panes(x,
           hspace=None,
           **kwargs):
     """
-    .. raw:: latex
+    Panes
+    -----
 
-        \subsubsection*{Inputs}
+    # Inputs
+    The panes function supports numerical inputs in the following forms:
+    |   x                              |   y                               |  result  |  notes                                          |
+    |  ---                             |  ---                              |  ---     |  ---                                            |
+    |  array                           |  array                            |  11      |                                                 |
+    |  array                           |  [array, array]                   |  12      |  Both `y`s share `x`                            |
+    |  [n*[array]]                     |  [n*[array]]                      |  1n      |  Each `y` has an `x`                            |
+    |  array                           |  [array, array]                   |  21      |  Both `y`s share `x`                            |
+    |  [array, array]                  |  [array, array]                   |  21      |  Each `y` has an `x`                            |
+    |  array                           |  [n*[array], n*[array]]           |  2n      |  All curves in all (2) panes share a single `x` |
+    |  [array, array]                  |  [n*[array], n*[array]]           |  2n      |  All curves in each pane share an `x`           |
+    |  [n*[array], n*[array]]          |  [n*[array], n*[array]]           |  2n      |  All curves in all (2) panes have their own `x` |
+    |  [n*[array], ... up to m]        |  [n*[array], ... up to m]         |  mn      |  All curves in all panes have their own `x`     |
 
-    The table below displays the supported numerical input combinations, where:
+    where
 
-    - array:  List or NumPy array with numerical values
-    - [...]:  List containing ...
-    - result: <panes><curves per pane>
+    * array:  List or NumPy array with numerical values
+    * [...]:  List containing ...
+    * result: <panes><curves per pane>
 
-    .. list-table:: Valid input combinations.
-        :widths: 25 25 10 40
-        :header-rows: 1
-
-        * - ``x``
-          - ``y``
-          - Result
-          - Notes
-        * - array
-          - array
-          - 11
-          - 
-        * - array
-          - [array, array]
-          - 12
-          - Both `y`s share a single `x`
-        * - [array, array]
-          - array
-          - 21
-          - Both `x`s share a single `y`
-        * - [n*[array]]
-          - [n*[array]]
-          - 1n
-          - Each `y` has an `x`
-        * - array
-          - [n*[array], n*[array]]
-          - 2n
-          - All curves in all (2) panes share a single `x`
-        * - [array, array]
-          - [n*[array], n*[array]]
-          - 2n
-          - All curves in each pane share an `x`
-        * - [n*[array], n*[array]]
-          - [n*[array], n*[array]]
-          - 2n
-          - All curves in all (2) panes have their own `x`
-        * - [n*[array], ... up to m]
-          - [n*[array], ... up to m]
-          - mn
-          - All curves in all panes have their own `x`
-
-    .. raw:: latex
-
-        \subsection*{Arguments}
-    
+    # Arguments
     Arguments are internally classified as FIGURE arguments, PLURAL arguments
     and CURVE arguments, namely:
 
-    - **Figure arguments**
-      Select few arguments which may be input only once in the plotting process, so as
-      to avoid conflicts. Ieg: passing `grid=True` twice (`plt.grid(...)`) will result
-      in no grid being drawn.
-      These are removed from the keyword arguments and used in the last `comparison` call.
+    * Figure arguments
+        Select few arguments which may be input only once in the plotting process, so as
+        to avoid conflicts. Ieg: passing `grid=True` twice (`plt.grid(...)`) will result
+        in no grid being drawn.
+        These are removed from the keyword arguments and used in the last `comparison` call.
 
-    - **Special arguments**
-      Select few arguments (ieg: `plot_labels`), which satisfy the condition of being
-      `lists with a length different to that of y` and which, for aesthetic purposes, 
-      must be applied only once.
-      In the case of `plot_labels`, if `plot_labels` is a list of length different to that
-      of `y`, it is assumed that the nth curve of each pane shares a label with the nth 
-      curve of all other panes and so a legend displaying the labels of the last pane will 
-      be displayed.
+    * Special arguments
+        Select few arguments (ieg: `plot_labels`), which satisfy the condition of being
+            `lists with a length different to that of y`
+        and which, for aesthetic purposes, must be applied only once.
 
-    - **Plural arguments**
-      Arguments with a keyword equal to any of the arguments which can be passed to the
-      `line` 2D plotter, in plural tense. The line plotter is chosen as it shares all 
-      general arguments with the other 2D plotter functions.
-      The plural arguments are assumed to be `lists of length equal to the number of panes`
-      and thus modify each pane. Ieg: x_tick_labels=[1, 2, 3] will set the tick labels
-      of the x axes to 1, 2 and 3 respectively in a 3-pane plot.
+        In the case of `plot_labels`, if `plot_labels` is a list of length different to that
+        of `y`, it is assumed that
+            - The nth curve of each pane shares a label with the nth curve of all other panes
+        and so a legend displaying the labels of the last pane will be displayed.
 
-    - **Curve arguments**
-      Curve arguments are passed as plurals to the comparison function, as they are
-      `lists with a length different to that of y` (thus they can't apply to each pane) 
-      and they are assumed to have a length equal to the number of curves in each plot.
+    * Plural arguments
+        Arguments with a keyword equal to any of the arguments which can be passed to the
+          `line`
+        2D plotter, in plural tense. The line plotter is chosen as it shares all general
+        arguments with the other 2D plotter functions.
+        The plural arguments are assumed to be
+          `lists of length equal to the number of panes`
+        and thus modify each pane. Ieg: x_tick_labels=[1, 2, 3] will set the tick labels
+        of the x axes to 1, 2 and 3 respectively in a 3-pane plot.
+
+    * Curve arguments
+        Curve arguments are passed as plurals to the comparison function, as they are
+          `lists with a length different to that of y`
+        (thus they can't apply to each pane) and they are assumed to have a length equal
+        to the number of curves in each plot.
+
 
     :type x:                list of list or list of np.ndarray or np.ndarray
     :type y:                list of list or list of np.ndarray or np.ndarray
@@ -131,7 +100,9 @@ def panes(x,
     :type kwargs:           any
     """
 
-    # Input check
+    ###############################
+    #       INPUT VALIDATION      #
+    ###############################
     single_x = (not isinstance(x[0], list) and len(x) == 1) or isinstance(x, np.ndarray)
     single_y = (not isinstance(y[0], list) and len(y) == 1) or isinstance(y, np.ndarray)
 
@@ -158,7 +129,9 @@ def panes(x,
                 assert all([len(curve) == len(y) for curve in x]), \
                     ValueError('The length of x and the curves in y does not match.')
 
-    # Plot number
+    ###############################
+    #         PLOT NUMBER         #
+    ###############################
     if not single_y and not single_x:
         if isinstance(y[0], list):
             n_plots = len(y)
@@ -171,19 +144,34 @@ def panes(x,
     elif single_x:
         n_plots = len(y) if not single_y else 1
 
-    # Figure arguments
-    fig_par = [                                                         # Get figure specific parameters
-                'backend',
-                'show',
-                'legend',
-                'legend_loc'
-              ]
-    fparams = list(set(fig_par) & set(list(kwargs.keys())))             # Intersection of kwarg keys and fig params
-    fparams = {k: kwargs.pop(k) for k in fparams}                       # Dictionary of figure parameters
+    ###############################
+    #          ARGUMENTS          #
+    ###############################
+    # keys ------------------------------------------------------------
+    fkeys   = [
+        'backend',
+        'show',
+        'legend',
+        'legend_loc'
+    ]
+    skeys   = [
+        'plot_label',
+        'plot_labels'
+    ]
 
-    # Plurals
-    params  = line.__init__.__code__.co_varnames                        # Get line function parameters
-    plurals = [param + 's' for param in params]                         # Parameters: in plural
+    # figure arguments ------------------------------------------------
+    fargs = {k: kwargs.pop(k) for k in list(set(fkeys) & set(list(kwargs.keys())))}
+
+    fargs['backend']    = fargs.pop('backend',    'Qt5Agg')
+    fargs['legend_loc'] = fargs.pop('legend_loc', (0.875, 0.55))
+
+    # legend arguments -----------------------------------------------
+    largs = {k: kwargs.pop(k) for k in list(kwargs.keys()) if k in skeys and len([kwargs[k]] if not isinstance(kwargs[k], list) else kwargs[k]) != n_plots}
+    fargs['legend'] = fargs.pop('legend',     len([k for k in largs.keys() if 'label' in k]) != 0)
+
+    # plural arguments ------------------------------------------------
+    args  = line.__init__.__code__.co_varnames                        # Get line function parameters
+    plurals = [param + 's' for param in args]                         # Parameters: in plural
     plurals = list(set(plurals) & set(list(kwargs.keys())))             # Intersection of kwargs keys and plurals
     plurals = {k: kwargs.pop(k) for k in plurals}                       # Dictionary of plurals
 
@@ -195,38 +183,23 @@ def panes(x,
         """
         return {k[:-1]: plurals[k][i] for k in list(plurals.keys())}
 
-    # Special
-    sparaml = [                                                         # Get special parameters
-                'plot_label',
-                'plot_labels'
-              ]
-    sparams = {k: v for k, v in kwargs.items() if k in sparaml and len([v] if not isinstance(v, list) else v) != n_plots}
-    sparams = {k: kwargs.pop(k) for k in sparams}                       # Dictionary of figure parameters
+    # curve arguments ------------------------------------------------
+    cargs = {k: plurals.pop(k) for k in list(plurals.keys()) if isinstance(plurals[k], list) and len(plurals[k]) != n_plots}
 
-    # Curve arguments
-    cparams = {k: v for k, v in plurals.items() if isinstance(v, list) and len(v) != n_plots}
-    for k in cparams.keys():
-        if k in plurals.keys():
-            plurals.pop(k)
-
-    # Plot defaults
-    fparams['backend']    = fparams.pop('backend',    'Qt5Agg')
-    fparams['legend']     = fparams.pop('legend',     len(set([p for p in sparaml if 'label' in p])
-                                                          &
-                                                          set(list({**sparams, **plurals}.keys())))
-                                                      != 0)
-    fparams['legend_loc'] = fparams.pop('legend_loc', (0.875, 0.55))
-
-    # Figure setup
+    ###############################
+    #           FIGURE            #
+    ###############################
     N = min(n_plots, ceil(n_plots/rows))
     M = rows
 
     height = 3.5 if M == 1 else 4
 
     if fig is None:
-        fig = figure((5 * N, height * M), backend=fparams['backend'])
+        fig = figure((5 * N, height * M), backend=fargs['backend'])
 
-    # Plot
+    ###############################
+    #            PLOT             #
+    ###############################
     for n in range(n_plots):
 
         ax_transient = plt.subplot2grid((M, N),
@@ -235,15 +208,15 @@ def panes(x,
                                         colspan=1)
 
         # Margins
-        plt.subplots_adjust(top = 0.88,
-                            bottom = 0.11,
-                            left=0.1                                if left   is not None else left,
-                            right=0.85 if M == 1 else 0.75          if right  is not None else right,
-                            wspace=0.6                              if wspace is not None else wspace,
-                            hspace=0.35                             if hspace is not None else hspace)
+        plt.subplots_adjust(top=     0.88,
+                            bottom=  0.11,
+                            left=    0.1                              if left   is not None else left,
+                            right=   0.85 if M == 1 else 0.75         if right  is not None else right,
+                            wspace=  0.6                              if wspace is not None else wspace,
+                            hspace=  0.35                             if hspace is not None else hspace)
 
         # Pass keyword arguments to last
-        args = {**kwargs, **plural(n), **cparams} if n != n_plots - 1 else {**kwargs, **plural(n), **cparams, **fparams, **sparams}
+        args = {**kwargs, **plural(n), **cargs} if n != n_plots - 1 else {**kwargs, **plural(n), **cargs, **fargs, **largs}
 
         # If y[n] is a list (multiple curves in each plot)
         if isinstance(y[n], list) and isinstance(x[n], list):
@@ -297,7 +270,7 @@ def panes(x,
                         wspace=  0.6                              if wspace is None else wspace,
                         hspace=  0.35                             if hspace is None else hspace)
 
-    if fparams['legend']:
+    if fargs['legend']:
 
         # Legend placement
         legend = (c for c in plt.gca().get_children() if isinstance(c, mpl.legend.Legend))
